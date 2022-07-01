a3aac16af9feb67b2bf31a0d2a38b1ba3f942908
frankandreace <francesco.andreace@gmail.com>
2022-07-01 15:16:12 +0200

    eliminating snps based on parameters

--- reduce_noSNPs.py
+++ reduce_noSNPs.py
@@ -1,7 +1,4 @@
 import argparse
-from platform import node
-
-from yaml import parse
 
 def parse_graph(gfaFile, verbose=False):
     graph = {}
@@ -32,70 +29,60 @@ def parse_graph(gfaFile, verbose=False):
     if verbose:
         print(f'Number of nodes {len(graph)}')
 
+
     return graph, nodelength, firstnode
 
-
-def breadth_first_search_removing_snp(node, graph,nodeslength,verbose=False):
-    stack = [node]
-    to_remove={} 
-    already_seen={}
+def remove_snp(starting_node,graph,nodes_length,snp_len=1,max_parallel_nodes=3,verbose=False):
+    stack=[starting_node]
+    to_remove={} # true->remove; false->already seen but not remove.
+    removed_num=0
 
     while len(stack)>0:
-        local_nodes = set()
-        current_node = stack.pop()
-        already_seen[current_node] = True
-        
 
+        current_node = stack.pop()
+        #print(f'visiting {current_node}')
+
+        if to_remove.get(current_node) != None:
+            for neighbor in graph[current_node]:
+                #print(f'might add {neighbor}')
+                if to_remove.get(neighbor) == None:
+                    stack.append(neighbor)
+            continue
+
+        stack.extend(graph[current_node])
+        
+        local_nodes=set()
         for neighbor in graph[current_node]:
-            if (neighbor in already_seen) and (not already_seen[neighbor]):
+            if to_remove.get(neighbor) == None:
                 local_nodes.add(neighbor)
 
-        
-        if len(local_nodes) != 2:
-            if already_seen.get(node) != None:
-                    stack.append(node)
-            continue
-        
-        for node in local_nodes:
-            if nodeslength[node] != 1:
-                if already_seen.get(node) != None:
-                    stack.append(node)
-                continue
-                
-        node1 , node2 = local_nodes
+        print(f"local_nodes {local_nodes} , to_remove_len {len(to_remove)}")
 
-        if graph[node1] != graph[node2]:
-            if already_seen.get(node) != None:
-                    stack.append(node)
-            continue
-        
-        neig_n1 = graph[node1]
-        neig_n2 = graph[node2]
+        if (len(local_nodes) <= max_parallel_nodes) and (len(local_nodes)>0):
+            if all(nodes_length[node]<= snp_len for node in local_nodes) and (all(len(graph[node])==2 for node in local_nodes)):
+                Keeping_node=local_nodes.pop()
+                if (all(graph[node]==graph[Keeping_node] for node in local_nodes)):
+                    to_remove[Keeping_node]=False
+                    for node in local_nodes:
+                        to_remove[node]=True
+                        removed_num+=1
 
-        neig_n1.pop(node)
-        neig_n2.pop(node)
-        if (len(neig_n1) == len(neig_n2)) and (len(neig_n1)==1):
-            nextnode=neig_n1.pop()
-            to_remove[node1] = True
-            stack.append(nextnode)
-            continue
 
-        for node in local_nodes:
-                if already_seen.get(node) != None:
-                    stack.append(node)
-    
-    return to_remove
+        to_remove[current_node]=False
+
+    return removed_num, to_remove
 
 def printgraph(gfaFile,outfile,to_remove):
-    with open(gfaFile, 'r') as gfa:
-        for line in gfa:
-            elements = line.strip().split('\t')
-            if elements[0] == 'S':
-                if to_remove.get(elements[1]) != None:
-                    print(line,file=outfile)
-            elif elements[0] == 'L':
-                if (to_remove.get(elements[1]) != None) and (to_remove.get(elements[3]) != None):
-                    print(line,file=outfile)
+    with open(outfile, 'w') as out:
+        with open(gfaFile, 'r') as gfa:
+            for line in gfa:
+                elements = line.strip().split('\t')
+                if elements[0] == 'S':
+                    if not to_remove.get(elements[1]):
+                        print(line,file=out,end='')
+                elif elements[0] == 'L':
+                    if (not to_remove.get(elements[1])) and (not to_remove.get(elements[3])):
+                        print(line,file=out,end='')
 
 
 
@@ -111,6 +98,9 @@ if __name__ == "__main__":
 
     graph, nodes_length, firstnode = parse_graph(args.graph_file,verbose=True)
     print('input finished')
-    nodes_to_remove = breadth_first_search_removing_snp(firstnode,graph,nodes_length,verbose=True)
+    #nodes_to_remove = breadth_first_search_removing_snp(firstnode,graph,nodes_length,verbose=True)
+    num_removed, nodes_to_remove = remove_snp(firstnode,graph,nodes_length,snp_len=5,verbose=True)
     print('removed finished')
     printgraph(args.graph_file,args.outfile,nodes_to_remove)
+
+    print(num_removed)
